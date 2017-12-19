333d9e9d350f6bcf7ced926dd27270e61308dc07
jose5918 <jose5918@gmail.com>
2017-12-19 09:33:34 -0800

    Detect integration branch in pull request list (#28)
    * Detect integration branch in pull request list
    
    * Add ref from pull request as branch

--- scripts/test_integration.py
+++ scripts/test_integration.py
@@ -18,39 +18,30 @@ class TestIntegration(unittest.TestCase):
     class PullRequestGetMock():
         def json(self):
             pull_request_response_example = r'''{
-	"body": "```\r\nintegration_branch: \"not_master\"\r\npull_requests:\r\n  - https://github.com/octocat/Hello-world/pull/123\r\n```",
+	"body": "```\r\npull_requests:\r\n  - https://github.com/octocat/Hello-world/pull/123\r\n```",
 	"head": {
 		"sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		"ref": "branch-name",
 		"repo": {
 			"owner": {
 				"login": "octocat"
 			},
-			"name": "Hello-World"
+			"name": "Hello-World",
+            "full_name": "octocat/Hello-World"
 		}
 	}
 }'''
             return json.loads(pull_request_response_example)
-    class BranchRequestGetMock():
-        def json(self):
-            branch_response_example = r'''{
-  "name": "master",
-  "commit": {
-    "sha": "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
-    "url": "https://api.github.com/repos/octocat/Hello-World/commits/7fd1a60b01f91b314f59955a4e4d4e80d8edf11d"
-  }
-}'''
-            return json.loads(branch_response_example)
+
 
     @patch('requests.get', return_value=PullRequestGetMock())
     def test_get_description_section(self, patch1):
         current_pull_request = integration.Pull_Request('https://github.com/octocat/Hello-world/pull/123')
         urls = current_pull_request.get_description_section('pull_requests', '```')
-        integration_branch = current_pull_request.get_description_section('integration_branch', '```') 
         self.assertEqual(
             urls,
             ['https://github.com/octocat/Hello-world/pull/123']
         )
-        self.assertEqual(integration_branch, 'not_master')
 
     def test_validate_url(self):
         test_urls = [
@@ -65,30 +56,27 @@ class TestIntegration(unittest.TestCase):
            integration.validate_url(test_urls[2],'^https?:\/\/github.com\/.*\/pull\/\d+\/?$')
 
         self.assertTrue('ERROR: Invalid url: {}'.format(test_urls[2]) in context.exception)
-  
-    @patch('requests.get', return_value=BranchRequestGetMock())
-    def test_validate_integration_branch(self, patch1):
-        integration.validate_integration_branch('octocat/Hello-world','master')
     
 
     @patch('requests.get', return_value=PullRequestGetMock())
     def test_generate_build_parameters(self, patch1):
         pull_requests = ['https://github.com/octocat/Hello-world/pull/123','https://github.com/octocat/Hello/pull/124']
         circle = CircleCI('integration/repo', 'foo')
-        integration.generate_build_parameters(circle, pull_requests,'https://api.github.com/repos/octocat/integration/statuses/sha')
+        integration.generate_build_parameters(circle, pull_requests)
         self.assertEqual(
             circle.build_param,
             {'PR_URL': ['https://github.com/octocat/Hello-world/pull/123', 'https://github.com/octocat/Hello/pull/124'],
              'CUSTOM_VALUES':
              '{ "Hello_World": {"repo": "Hello-World","tag": "6dcb09b5b57875f334f61aebed695e2e4193db5e"},"Hello_World": {"repo": "Hello-World","tag": "6dcb09b5b57875f334f61aebed695e2e4193db5e"} }',
-             'STATUS_URL': 'https://api.github.com/repos/octocat/integration/statuses/sha,https://api.github.com/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e,https://api.github.com/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e'}
+             'STATUS_URL': 'https://api.github.com/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e,https://api.github.com/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e'}
         )
-        circle_master = CircleCI('integration/repo', 'master')
-        integration.generate_build_parameters(circle_master, pull_requests,'https://api.github.com/repos/octocat/integration/statuses/sha')
+        #Ensure that we ignore status for integration repo
+        circle_master = CircleCI('octocat/hello-world', 'master')
+        integration.generate_build_parameters(circle_master, pull_requests)
         self.assertEqual(
             circle_master.build_param,
             {'PR_URL': ['https://github.com/octocat/Hello-world/pull/123', 'https://github.com/octocat/Hello/pull/124'],
-             'CUSTOM_VALUES':
-             '{ "Hello_World": {"repo": "Hello-World","tag": "6dcb09b5b57875f334f61aebed695e2e4193db5e"},"Hello_World": {"repo": "Hello-World","tag": "6dcb09b5b57875f334f61aebed695e2e4193db5e"} }',
-             'STATUS_URL': 'https://api.github.com/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e,https://api.github.com/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e'}
+             'CUSTOM_VALUES': '',
+             'STATUS_URL': ''}
         )
+        self.assertEqual(circle_master.branch,'branch-name')
