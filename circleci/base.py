be39806eaaa4b9a63905312c458de22a07fd235e
Nan Liu <nan.liu@gmail.com>
2018-01-11 09:34:06 -0800

    Use httpauth instead of encoding headers.

--- circleci/base.py
+++ circleci/base.py
@@ -1,37 +1,40 @@
-import base64
 import os
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 class CircleCIBase():
+    # https://circleci.com/docs/api/v1-reference/
     def __init__(self):
         token = os.environ.get('CIRCLE_TOKEN')
         if token is None:
             raise Exception("Must set env var CIRCLE_TOKEN")
-        self._auth = base64.standard_b64encode('{}:'.format(token))
+        self.auth = HTTPBasicAuth(token, '')
         self.github_url = 'https://circleci.com/api/v1.1/project/github'
 
-    def post(self, url, data):
+    def request(self, verb, url, data=None):
         headers = {
-            'Authorization': 'Basic {}'.format(self._auth),
             'Content-Type': 'application/json'
         }
-        resp = requests.post(url, data=data, headers=headers)
-        return resp.json()
+        if verb == 'get':
+            resp = requests.get(url, auth=self.auth, headers=headers)
+        elif verb == 'post':
+            resp = requests.post(url, auth=self.auth, headers=headers, data=data)
 
-    def get(self, url):
-        headers = {
-            'Authorization': 'Basic {}'.format(self._auth),
-            'Content-Type': 'application/json'
-        }
-        resp = requests.get(url, headers=headers)
-        if resp.status_code == 200:
+        if 200 <= resp.status_code < 300:
             return resp.json()
         else:
             print resp.text
 
+    def get(self, url):
+        return self.request('get', url)
+
+    def post(self, url, data):
+        return self.request('post', url, data=data)
+
     # get running builds
     def get_build_status(self, repo, branch=None, filter='running', limit=30):
+        # GET: /project/:vcs-type/:username/:project
         if branch is not None:
             repo = "{}/tree/{}".format(repo, branch)
         url = '{}/{}?filter={}&limit={}'.\
@@ -39,14 +42,28 @@ class CircleCIBase():
         return self.get(url)
 
     def get_single_build_status(self, repo, build_num):
+        # GET: /project/:vcs-type/:username/:project/:build_num
         url = '{}/{}/{}'.\
             format(self.github_url, repo, build_num)
         return self.get(url)
 
     # trigger a new build in circleci
     def trigger_build(self, repo, branch, data):
-        # NOTE: https://circleci.com/docs/api/v1-reference/#new-build-branch
+        # https://circleci.com/docs/api/v1-reference/#new-build-branch
+        # POST: /project/:vcs-type/:username/:project/tree/:branch
         url = '{}/{}/tree/{}'.\
             format(self.github_url, repo, branch)
-        result = self.post(url, data)
-        return result
+        resp = self.post(url, data)
+        self.build_num = resp['build_num']
+        self.build_url = resp['build_url']
+        # print "Build Number: {}".format(self._build_num)
+        return resp
+
+    def github_status_pending(self, context=None, url=None):
+        if self.build_url is None:
+            raise Exception('No build has been triggered.')
+        if context is None:
+            context = 'ci/circleci-integration'
+        desc = 'The integration build {} started'.format(self._build_num)
+
+        GithubStatus().create_status('pending', self.build_url, desc, context, url=url)
